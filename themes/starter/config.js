d69cb8b209f56926c4d859d1177bf5e01264c33b
KaterinaJiang <xiaoxuanjiang22@gmail.com>
2024-11-01 16:22:54 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -240,12 +240,12 @@ const CONFIG = {
 
   // 联系模块
   STARTER_CONTACT_ENABLE: true, // 联系模块开关
-  STARTER_CONTACT_TITLE: '联系我们',
-  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
-  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
-  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+  STARTER_CONTACT_TITLE: 'Contact Us',
+  STARTER_CONTACT_TEXT: 'Tell us the problem you encountered',
+  STARTER_CONTACT_LOCATION_TITLE: 'Our location',
+  STARTER_CONTACT_LOCATION_TEXT: 'Hangzhou, China',
+  STARTER_CONTACT_EMAIL_TITLE: 'How can we assist you？',
+  STARTER_CONTACT_EMAIL_TEXT: 'ChinAfrica520@hotmail.com',
 
   // 嵌入外部表单
   STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
