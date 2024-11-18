eddc87dba608322ead69e00b82d7df59f3b758d3
KaterinaJiang <xiaoxuanjiang22@gmail.com>
2024-11-18 18:20:10 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -111,139 +111,139 @@ STARTER_FEATURE_4_BUTTON_URL: 'https://docs.tangly1024.com/about',  // 特性4
 
   // 首页用户测评区块
   STARTER_TESTIMONIALS_ENABLE: false, // 测评区块开关
-  STARTER_TESTIMONIALS_TITLE: 'Customer feedback',
-  STARTER_TESTIMONIALS_TEXT_1: 'What customers say',
+  STARTER_TESTIMONIALS_TITLE: '用户反馈',
+  STARTER_TESTIMONIALS_TEXT_1: '我们的用户怎么说',
   STARTER_TESTIMONIALS_TEXT_2:
-    'Hundreds of customers choose to use Chinafrica to transport their oods, we help them complete luggage transportation, company procurement, bulk purchase...',
+    '数千位站长选择用NotionNext搭建他们的网站,通过帮助手册、交流社群以及技术咨询，大家成功上线了自己的网站',
   STARTER_TESTIMONIALS_STAR_ICON: '/images/starter/testimonials/icon-star.svg', // 评分图标
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   STARTER_TESTIMONIALS_ITEMS: [
     {
       STARTER_TESTIMONIALS_ITEM_TEXT:
-        'As a business owner in SA, finding a reliable freightforwarder is crucial. Chinafrica has been a game-changerfor us! ',
+        '感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ ',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Thandiweauthor',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'South Africa',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Ryan_G',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log 站长',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://blog.gaoran.xyz/'
     },
     {
       STARTER_TESTIMONIALS_ITEM_TEXT:
-        'Chinafrica has made international shipping a breeze for us.Their team is knowledgeable, and they handle all the details,making the process stress-free.',
+        '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Wassana',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Thailand',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
     },
     {
       STARTER_TESTIMONIALS_ITEM_TEXT:
-        'Chinafrica has been an invaluable partner for our companyTheir expertise in international shipping has saved us timeand money.',
+        '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F6c096b44-beb9-48ee-8f92-1efdde47f3a3%2F338962f1-d352-49c7-9a1b-746e35a7005c%2Fhf.png?table=block&id=ce5a48a9-d77a-4843-a3d9-a78cd4f794ce&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Oliver',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'UK',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
     },
     {
       STARTER_TESTIMONIALS_ITEM_TEXT:
-        'They are always on time, and their customer support isexcellent. lt is reassuring to know that my shipments arealways in good hands.',
+        '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Johnson',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'USA',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
     },
     {
       STARTER_TESTIMONIALS_ITEM_TEXT:
-        'I highly recommend them to anyone in need of a reliablefreight forwarding service!',
+        '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fafb21381-f51b-4fd0-9998-800dbeb64dbe%2Favatar.png?table=block&id=195935d2-0d8d-49fc-bd81-1db42ee50840&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Lerato',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'South Africa',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
     },
     {
-      STARTER_TESTIMONIALS_ITEM_TEXT: 'They are efficient, reliable, and their prices are very fair！',
+      STARTER_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
       STARTER_TESTIMONIALS_ITEM_AVATAR:
         'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fe4f391d7-7d65-4c05-a82c-c6e2c40f06e4%2Fa2a7641a26b367608c6ef28ce9b7e983_(2).png?table=block&id=a386eb0e-4c07-4b18-9ece-bba4e79ce21c&spaceId=6c096b44-beb9-48ee-8f92-1efdde47f3a3&width=100&userId=27074aef-7216-41ed-baef-d9b53addd870&cache=v2',
-      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'Doe',
-      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'USA',
-      STARTER_TESTIMONIALS_ITEM_URL: ''
+      STARTER_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
+      STARTER_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
+      STARTER_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
     }
   ],
 
   //   FAQ 常见问题模块
   STARTER_FAQ_ENABLE: true, // 常见问题模块开关
-  STARTER_FAQ_TITLE: 'FAQ',
-  STARTER_FAQ_TEXT_1: 'Have any questions?',
-  STARTER_FAQ_TEXT_2: 'We have collected some frequently asked questions from users.',
+  STARTER_FAQ_TITLE: '常见问题解答',
+  STARTER_FAQ_TEXT_1: '有任何问题吗？请看这里',
+  STARTER_FAQ_TEXT_2: '我们收集了常见的用户疑问',
 
-  STARTER_FAQ_1_QUESTION: 'Can l ship cellphones?？',
+  STARTER_FAQ_1_QUESTION: 'NotionNext有帮助文档吗？',
   STARTER_FAQ_1_ANSWER:
-    'We provide cellphone shipping service, but it can only be delivered by Express, and thereis no compensation if it is lost, so we do not recommend that you ship your phone.',
+    'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署',
 
-  STARTER_FAQ_2_QUESTION: 'Can l send brand good?',
+  STARTER_FAQ_2_QUESTION: '部署后要如何编写文章？',
   STARTER_FAQ_2_ANSWER:
-    'Generally speaking, branded products can only be transported by sea (branded productsdo not include luxury goods and well-known brands, such as Nike and Adidas)',
+    '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>',
 
-  STARTER_FAQ_3_QUESTION: 'What are your charges?',
+  STARTER_FAQ_3_QUESTION: '站点部署失败，更新失败？',
   STARTER_FAQ_3_ANSWER:
-    'Our fees consist of two parts: shipping and service fees (service fees include customsclearance fees and courier fees within the destination country)',
+    '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助',
 
-  STARTER_FAQ_4_QUESTION: 'Why does shipping take so long?',
+  STARTER_FAQ_4_QUESTION: '文章没有实时同步？',
   STARTER_FAQ_4_ANSWER:
-    'Due to the long distance of international shipping, there are often some uncontrollable conditions (such as weather factors, customs clearance inspection factors, etc.). in this case, your package may be slightly delayed in arriving at your side.',
+    '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决',
 
   // 团队成员区块
   STARTER_TEAM_ENABLE: true, // 团队成员区块开关
-  STARTER_TEAM_TITLE: 'Main shipping destinations',
-  STARTER_TEAM_TEXT_1: 'Our destinations',
+  STARTER_TEAM_TITLE: '团队成员',
+  STARTER_TEAM_TEXT_1: '我们的开发者团队',
   STARTER_TEAM_TEXT_2:
-    'We mainly transport goods from China to these regions Other countries please contact our customer support.',
+    'NotionNext 由众多开源技术爱好者们共同合作完成，感谢每一位<a className="underline" href="https://github.com/tangly1024/NotionNext/graphs/contributors">贡献者</a>',
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   STARTER_TEAM_ITEMS: [
     {
       STARTER_TEAM_ITEM_AVATAR:
-        '/images/starter/team/team-01.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Africa',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'South Africa, Zimbabwe...'
+        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fa06c61bb-980e-4180-bc18-c15f92c78bb4%2Ftangly1024.jpg?table=collection&id=8e7acf17-de09-4fa1-abde-b5b80ad4a813&t=8e7acf17-de09-4fa1-abde-b5b80ad4a813&width=100&cache=v2',
+      STARTER_TEAM_ITEM_NICKNAME: 'Tangly',
+      STARTER_TEAM_ITEM_DESCRIPTION: 'Developer'
+    },
+    {
+      STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-01.png',
+      STARTER_TEAM_ITEM_NICKNAME: 'Melissa Tatcher',
+      STARTER_TEAM_ITEM_DESCRIPTION: 'Marketing Expert'
     },
     {
       STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-02.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Asia',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Thailand, Malaysia...'
+      STARTER_TEAM_ITEM_NICKNAME: 'Stuard Ferrel',
+      STARTER_TEAM_ITEM_DESCRIPTION: 'Digital Marketer'
     },
     {
       STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-03.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'Europe',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'UK, France...'
-    },
-    {
-      STARTER_TEAM_ITEM_AVATAR: '/images/starter/team/team-04.png',
-      STARTER_TEAM_ITEM_NICKNAME: 'America and others',
-      STARTER_TEAM_ITEM_DESCRIPTION: 'Ask us for details'
+      STARTER_TEAM_ITEM_NICKNAME: 'Eva Hudson',
+      STARTER_TEAM_ITEM_DESCRIPTION: 'Creative Designer'
     }
   ],
 
   // 博客文章区块
   STARTER_BLOG_ENABLE: true, // 首页博文区块开关
-  STARTER_BLOG_TITLE: 'Our blog',
+  STARTER_BLOG_TITLE: '我们的博客',
   STARTER_BLOG_COUNT: 3, // 首页博文区块默认展示前3篇文章
-  STARTER_BLOG_TEXT_1: 'Recent announcements',
+  STARTER_BLOG_TEXT_1: '最近的新闻',
   STARTER_BLOG_TEXT_2:
-    'Here we will publish the latest news about Chinafrica, including thesituation of international logistics, price changes, new suppliers, etc.',
+    '这里会发布一些关于NotionNext的最新动态，包括新的动向、新的未来计划，以及新功能的特性',
 
   // 联系模块
   STARTER_CONTACT_ENABLE: true, // 联系模块开关
-  STARTER_CONTACT_TITLE: 'Contact us',
-  STARTER_CONTACT_TEXT: 'Tell us theproblem you encountered',
-  STARTER_CONTACT_LOCATION_TITLE: 'Our location',
-  STARTER_CONTACT_LOCATION_TEXT: 'Hangzhou, China',
-  STARTER_CONTACT_EMAIL_TITLE: 'How can we assist you?？',
-  STARTER_CONTACT_EMAIL_TEXT: 'chinafrica520@hotmail.com',
+  STARTER_CONTACT_TITLE: '联系我们',
+  STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
+  STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
+  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
+  STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
+  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
 
   // 嵌入外部表单
   STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
@@ -291,68 +291,67 @@ STARTER_FEATURE_4_BUTTON_URL: 'https://docs.tangly1024.com/about',  // 特性4
     }
   ],
 
-  STARTER_FOOTER_SLOGAN: 'Make the best choice for you.',
+  STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
 
   // 页脚三列菜单组
   STARTER_FOOTER_LINK_GROUP: [
     {
-      TITLE: 'About us',
+      TITLE: '关于我们',
       LINK_GROUP: [
-        { TITLE: 'Home', URL: '/#home' },
-        { TITLE: 'Our story', URL: 'https://docs.tangly1024.com/about' },
+        { TITLE: '官方主页', URL: '/#home' },
+        { TITLE: '操作文档', URL: 'https://docs.tangly1024.com/about' },
         {
-          TITLE: 'What we do',
+          TITLE: '帮助支持',
           URL: 'https://docs.tangly1024.com/article/how-to-question'
         },
         {
-          TITLE: 'Contact us',
+          TITLE: '合作申请',
           URL: 'https://docs.tangly1024.com/article/my-service'
         }
       ]
     },
     {
-      TITLE: 'About service',
+      TITLE: '功能特性',
       LINK_GROUP: [
         {
-          TITLE: 'Our rates',
+          TITLE: '部署指南',
           URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next'
         },
         {
-          TITLE: 'Areas we ship',
+          TITLE: '升级指南',
           URL: 'https://docs.tangly1024.com/article/how-to-update-notionnext'
         },
-        { TITLE: 'Products we ship', URL: 'https://docs.tangly1024.com/article/latest' }
+        { TITLE: '最新版本', URL: 'https://docs.tangly1024.com/article/latest' }
       ]
     },
     {
-      TITLE: 'Cooperate',
+      TITLE: 'Notion写作',
       LINK_GROUP: [
         {
-          TITLE: 'For Chinese merchants',
+          TITLE: 'Notion开始写作',
           URL: 'https://docs.tangly1024.com/article/start-to-write'
         },
         {
-          TITLE: 'For logistics companies',
+          TITLE: '快捷键提升效率',
           URL: 'https://docs.tangly1024.com/article/notion-short-key'
         },
         {
-          TITLE: 'For corporate customers',
+          TITLE: '中国大陆使用Notion',
           URL: 'https://docs.tangly1024.com/article/notion-faster'
         }
       ]
     }
   ],
 
-  STARTER_FOOTER_BLOG_LATEST_TITLE: 'Latest Announcement',
+  STARTER_FOOTER_BLOG_LATEST_TITLE: '最新文章',
 
-  STARTER_FOOTER_PRIVACY_POLICY_TEXT: 'Privacy policy',
+  STARTER_FOOTER_PRIVACY_POLICY_TEXT: '隐私政策',
   STARTER_FOOTER_PRIVACY_POLICY_URL: '/privacy-policy',
 
-  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: 'Legal Notice',
+  STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_TEXT: '法律声明',
   STARTER_FOOTER_PRIVACY_LEGAL_NOTICE_URL: '/legacy-notice',
 
-  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: 'Service Agreement
-',
+  STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_TEXT: '服务协议',
   STARTER_FOOTER_PRIVACY_TERMS_OF_SERVICE_URL: '/terms-of-use',
 
   // 404页面的提示语
