05ff8e94f35a889ff1e668b08826e8bed00f615a
KaterinaJiang <xiaoxuanjiang22@gmail.com>
2026-04-16 16:41:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -28,7 +28,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ChinAfrica', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '-express- Trading And Logistics Company', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '-express- Trading And Logistics Company ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.chin-a-frica.store', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ChinAfrica, Trading And Logistics Company', // 网站关键词 英文逗号隔开
 
